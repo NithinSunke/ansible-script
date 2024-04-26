3b070dd315df2162ee1bea2bc239a9fd8601742a
Nithin SUnke <sunkenithin@gmail.com>
2024-04-26 22:49:36 +0000

    asd

---
+++ orcl19c/prepare_host.sh
@@ -0,0 +1,10 @@
+. /tmp/scripts/orcl19c/install.env
+
+echo "setting hostname"
+echo "======================================"
+hostnamectl set-hostname ${HOSTNAME}
+
+echo "******************************************************************************"
+echo "Set root and oracle password and change ownership of /u01." `date`
+echo "******************************************************************************"
+echo -e "${ROOT_PASSWORD}\n${ROOT_PASSWORD}" | passwd
--- orcl19c/root_setup.sh
+++ orcl19c/root_setup.sh
@@ -1,18 +1,6 @@
-. /vagrant/config/install.env
+sh /tmp/scripts/orcl19c/prepare_disks.sh
 
-echo "setting hostname"
-echo "======================================"
-hostnamectl set-hostname ${HOSTNAME}
-
-echo "******************************************************************************"
-echo "Set root and oracle password and change ownership of /u01." `date`
-echo "******************************************************************************"
-echo -e "${ROOT_PASSWORD}\n${ROOT_PASSWORD}" | passwd
-
-
-sh /vagrant/scripts/prepare_disks.sh
-
-sh /vagrant/scripts/install_os_packages.sh
+sh /tmp/scripts/orcl19c/install_os_packages.sh
 echo -e "${ORACLE_PASSWORD}\n${ORACLE_PASSWORD}" | passwd oracle
 
 mkdir -p ${SCRIPTS_DIR}
