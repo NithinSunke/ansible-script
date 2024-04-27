607976e9d638d881d8f74295ffd2c014dd5f4221
Nithin SUnke <sunkenithin@gmail.com>
2024-04-27 01:14:11 +0000

    sdef

--- orcl19c/root_setup.sh
+++ orcl19c/root_setup.sh
@@ -1,16 +1,4 @@
-. /vagrant/config/install.env
 
-echo "setting hostname"
-echo "======================================"
-hostnamectl set-hostname ${HOSTNAME}
-
-echo "******************************************************************************"
-echo "Set root and oracle password and change ownership of /u01." `date`
-echo "******************************************************************************"
-echo -e "${ROOT_PASSWORD}\n${ROOT_PASSWORD}" | passwd
-
-
-sh /vagrant/scripts/prepare_disks.sh
 
 sh /vagrant/scripts/install_os_packages.sh
 echo -e "${ORACLE_PASSWORD}\n${ORACLE_PASSWORD}" | passwd oracle
